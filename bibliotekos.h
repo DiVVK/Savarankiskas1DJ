6c3547314799fddac36574b88c0635e887ccd579
DiVVK <147093207+DiVVK@users.noreply.github.com>
2023-12-27 16:47:55 +0200

    list<Studentas>

---
+++ bibliotekos.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <fstream>
+#include <chrono>
+#include <deque>
+#include <list>
+
+using std::cin;
+using std::cout;
+using std::istream;
+using std::ostream;
+using std::endl;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::left;
+using std::right;
+using std::string;
+using std::to_string;
+using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::getline;
+using std::stringstream;
+using std::exception;
+using std::deque;
+using std::list;
+using namespace std::chrono;
+
+#include "Studentas.h"
+#include "Funkcijos.h"
+#include "IsFailo.h"
+#include "FailuKurimas.h"
+#include "Skirstymas.h"
